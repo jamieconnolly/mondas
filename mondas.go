3fc21fb5ff06e6a131ccc3a087c4df45a39f4b07
Jamie Connolly <jamie@jamieconnolly.com>
2016-09-27 14:22:58 +0100

    Handle the parsing of flags, and move to `log.Fatalf`

--- mondas.go
+++ mondas.go
@@ -1,27 +1,37 @@
 package mondas
 
 import (
-	"fmt"
+	"flag"
+	"io/ioutil"
+	"log"
 	"os"
 	"path/filepath"
 	"syscall"
 )
 
 func Run() {
-	if len(os.Args) < 2 {
-		fmt.Fprintf(os.Stderr, "Usage: %s <command> [<args>]\n", os.Args[0])
-		os.Exit(1)
+	log.SetFlags(0)
+
+	flags := flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
+	flags.SetOutput(ioutil.Discard)
+
+	if err := flags.Parse(os.Args[1:]); err != nil {
+		log.Fatalf("%s: %v", os.Args[0], err)
+	}
+
+	if flags.NArg() == 0 {
+		log.Fatalf("Usage: %s <command> [<args>]", os.Args[0])
 	}
 
 	binDir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
 
-	cmdName := filepath.Base(os.Args[0]) + "-" + os.Args[1]
+	cmdName := filepath.Base(os.Args[0]) + "-" + flags.Arg(0)
 	cmdPath := filepath.Join(binDir, "../libexec", cmdName)
 
 	args := []string{cmdPath}
-	args = append(args, os.Args[2:]...)
+	args = append(args, flags.Args()[1:]...)
 
 	if err := syscall.Exec(cmdPath, args, os.Environ()); err != nil {
-		fmt.Fprintf(os.Stderr, "%s: %s\n", cmdName, err)
+		log.Fatalf("%s: %v", cmdName, err)
 	}
 }
