1284451e47339aa10d3b8e1d8ed874604c1c923a
Jamie Connolly <76968+jamieconnolly@users.noreply.github.com>
2016-09-28 18:15:33 +0100

    Split out the logic for running a command into `Command.Run`

---
+++ command.go
@@ -0,0 +1,18 @@
+package mondas
+
+import (
+	"os"
+	"syscall"
+)
+
+type Command struct {
+	Name string
+	Path string
+}
+
+func (c *Command) Run(arguments []string) error {
+	args := []string{c.Path}
+	args = append(args, arguments...)
+
+	return syscall.Exec(c.Path, args, os.Environ())
+}
