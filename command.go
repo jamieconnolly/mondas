88133a86ad3fa3f7c8d180bae047467187e02ee8
Jamie Connolly <jamie@jamieconnolly.com>
2016-10-03 22:45:22 +0100

    Parse the commands to find the summary

--- command.go
+++ command.go
@@ -1,17 +1,38 @@
 package mondas
 
 import (
+	"bufio"
 	"os"
+	"regexp"
+	"strings"
 	"syscall"
 )
 
 type Command struct {
 	Name string
 	Path string
+	Summary string
+}
+
+func (c *Command) Parse() error {
+	file, err := os.Open(c.Path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		summaryRegexp := regexp.MustCompile("^# Summary: (.*)$")
+		summaryMatch := summaryRegexp.FindStringSubmatch(scanner.Text())
+		if summaryMatch != nil {
+			c.Summary = strings.TrimSpace(summaryMatch[1])
+		}
+	}
+	return scanner.Err()
 }
 
 func (c *Command) Run(arguments []string) error {
 	args := append([]string{c.Path}, arguments...)
-
 	return syscall.Exec(c.Path, args, os.Environ())
 }
