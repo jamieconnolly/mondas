2729abd9ee2abb531552b48d74170b5f0231180a
Jamie Connolly <jamie@jamieconnolly.com>
2016-10-24 15:28:22 +0100

    Split out the executable logic into ExecCommand

--- commands.go
+++ commands.go
@@ -1,91 +1,29 @@
 package mondas
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"regexp"
 	"sort"
 	"strings"
-	"syscall"
 )
 
-type Command struct {
-	Name string
-	Path string
-	Summary string
-	Usage []string
-}
-
-func NewCommand(name string, path string) *Command {
-	return &Command{
-		Name: name,
-		Path: path,
-	}
-}
-
-func (c *Command) Parse() error {
-	file, err := os.Open(c.Path)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		summaryRegexp := regexp.MustCompile("^# Summary: (.*)$")
-		summaryMatch := summaryRegexp.FindStringSubmatch(scanner.Text())
-		if summaryMatch != nil {
-			c.Summary = strings.TrimSpace(summaryMatch[1])
-		}
-
-		usageRegexp := regexp.MustCompile("^# Usage: (.*)$")
-		usageMatch := usageRegexp.FindStringSubmatch(scanner.Text())
-		if usageMatch != nil {
-			c.Usage = append(c.Usage, strings.TrimSpace(usageMatch[1]))
-		}
-	}
-	return scanner.Err()
-}
-
-func (c *Command) Run(arguments []string) error {
-	for _, arg := range arguments {
-		switch(arg) {
-		case "--help", "-h":
-			return c.ShowHelp()
-		}
-	}
-
-	args := append([]string{c.Path}, arguments...)
-	return syscall.Exec(c.Path, args, os.Environ())
-}
-
-func (c *Command) ShowHelp() error {
-	c.Parse()
-
-	fmt.Println("Name:")
-	fmt.Printf("   %s - %s\n", c.Name, c.Summary)
-
-	if len(c.Usage) > 0 {
-		fmt.Println("\nUsage:")
-		for _, use := range c.Usage {
-			fmt.Printf("   %s\n", use)
-		}
-	}
-
-	return nil
+type Command interface {
+	LoadHelp() error
+	Name() string
+	Run(arguments []string) error
+	ShowHelp() error
+	Summary() string
+	Usage() []string
 }
 
 const MaxSuggestionDistance = 3
 
-type Commands []*Command
+type Commands []Command
 
 func (c Commands) Len() int {
 	return len(c)
 }
 
 func (c Commands) Less(i, j int) bool {
-	return c[i].Name < c[j].Name
+	return c[i].Name() < c[j].Name()
 }
 
 func (c Commands) Sort() Commands {
@@ -96,8 +34,8 @@ func (c Commands) Sort() Commands {
 func (c Commands) SuggestionsFor(typedName string) Commands {
 	suggestions := Commands{}
 	for _, cmd := range c {
-		suggestForDistance := stringDistance(typedName, cmd.Name) <= MaxSuggestionDistance
-		suggestForPrefix := strings.HasPrefix(strings.ToLower(cmd.Name), strings.ToLower(typedName))
+		suggestForDistance := stringDistance(typedName, cmd.Name()) <= MaxSuggestionDistance
+		suggestForPrefix := strings.HasPrefix(strings.ToLower(cmd.Name()), strings.ToLower(typedName))
 		if suggestForDistance || suggestForPrefix {
 			suggestions = append(suggestions, cmd)
 		}
---
+++ exec.go
@@ -0,0 +1,88 @@
+package mondas
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+	"syscall"
+)
+
+type ExecCommand struct {
+	name string
+	path string
+	summary string
+	usage []string
+}
+
+func NewExecCommand(name string, path string) *ExecCommand {
+	return &ExecCommand{
+		name: name,
+		path: path,
+	}
+}
+
+func (c *ExecCommand) LoadHelp() error {
+	file, err := os.Open(c.path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		summaryRegexp := regexp.MustCompile("^# Summary: (.*)$")
+		summaryMatch := summaryRegexp.FindStringSubmatch(scanner.Text())
+		if summaryMatch != nil {
+			c.summary = strings.TrimSpace(summaryMatch[1])
+		}
+
+		usageRegexp := regexp.MustCompile("^# Usage: (.*)$")
+		usageMatch := usageRegexp.FindStringSubmatch(scanner.Text())
+		if usageMatch != nil {
+			c.usage = append(c.usage, strings.TrimSpace(usageMatch[1]))
+		}
+	}
+	return scanner.Err()
+}
+
+func (c *ExecCommand) Name() string {
+	return c.name
+}
+
+func (c *ExecCommand) Run(arguments []string) error {
+	for _, arg := range arguments {
+		switch(arg) {
+		case "--help", "-h":
+			return c.ShowHelp()
+		}
+	}
+
+	args := append([]string{c.path}, arguments...)
+	return syscall.Exec(c.path, args, os.Environ())
+}
+
+func (c *ExecCommand) ShowHelp() error {
+	c.LoadHelp()
+
+	fmt.Println("Name:")
+	fmt.Printf("   %s - %s\n", c.Name(), c.Summary())
+
+	if len(c.Usage()) > 0 {
+		fmt.Println("\nUsage:")
+		for _, use := range c.Usage() {
+			fmt.Printf("   %s\n", use)
+		}
+	}
+
+	return nil
+}
+
+func (c *ExecCommand) Summary() string {
+	return c.summary
+}
+
+func (c *ExecCommand) Usage() []string {
+	return c.usage
+}
