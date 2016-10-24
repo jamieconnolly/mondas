e4ab952d51e621208f0d5968cd0526edf2a58db0
Jamie Connolly <76968+jamieconnolly@users.noreply.github.com>
2016-10-24 16:32:48 +0100

    Add in AddCommand and SetHelpCommand global functions

--- mondas.go
+++ mondas.go
@@ -8,6 +8,10 @@ import (
 
 var CommandLine = NewApp(filepath.Base(os.Args[0]))
 
+func AddCommand(cmd Command) {
+	CommandLine.AddCommand(cmd)
+}
+
 func Run() {
 	log.SetFlags(0)
 	log.SetPrefix(CommandLine.Name() + ": ")
@@ -15,4 +19,8 @@ func Run() {
 	if err := CommandLine.Init().Run(os.Args[1:]); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func SetHelpCommand(cmd Command) {
+	CommandLine.SetHelpCommand(cmd)
 }
