2a13df35328c9d4a0071922d26ea9e8d0680d605
Jamie Connolly <76968+jamieconnolly@users.noreply.github.com>
2017-01-18 11:46:34 +0000

    :hammer: Set the default LibexecDir inside NewApp

--- mondas.go
+++ mondas.go
@@ -7,31 +7,33 @@ import (
 
 	"github.com/jamieconnolly/mondas/cli"
 	"github.com/jamieconnolly/mondas/commands"
-	"github.com/kardianos/osext"
 )
 
 var (
-	CommandLine      = cli.NewApp(Name)
-	Executable, _    = osext.Executable()
-	ExecutablePrefix = filepath.Base(Executable) + "-"
-	LibexecDir       = filepath.Join(Executable, "../../libexec")
-	Name             = filepath.Base(Executable)
-	Version          = ""
+	// CommandLine is the default application.
+	CommandLine = cli.NewApp(Name, Version)
+	// HelpCommand is the default help command.
+	HelpCommand = commands.HelpCommand
+	// Name is the default name of the application.
+	Name = filepath.Base(os.Args[0])
+	// Version is the default version of the application.
+	Version = ""
+	// VersionCommand is the default version command.
+	VersionCommand = commands.VersionCommand
 )
 
+// AddCommand adds a command to the default application.
 func AddCommand(cmd *cli.Command) {
 	CommandLine.AddCommand(cmd)
 }
 
+// Run runs the default application using the arguments from os.Args.
 func Run() {
 	log.SetFlags(0)
 	log.SetPrefix(CommandLine.Name + ": ")
 
-	CommandLine.ExecutablePrefix = ExecutablePrefix
-	CommandLine.HelpCommand = commands.HelpCommand
-	CommandLine.LibexecDir = LibexecDir
-	CommandLine.Version = Version
-	CommandLine.VersionCommand = commands.VersionCommand
+	CommandLine.HelpCommand = HelpCommand
+	CommandLine.VersionCommand = VersionCommand
 
 	if err := CommandLine.Run(os.Args[1:]); err != nil {
 		log.Fatal(err)
---
+++ mondas_test.go
@@ -0,0 +1,21 @@
+package mondas_test
+
+import (
+	"testing"
+
+	"github.com/jamieconnolly/mondas"
+	"github.com/jamieconnolly/mondas/cli"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAddCommand(t *testing.T) {
+	cmd1 := &cli.Command{Name: "one"}
+	cmd2 := &cli.Command{Name: "two"}
+	cmds := cli.Commands{cmd1}
+	mondas.CommandLine = &cli.App{Commands: cmds}
+
+	mondas.AddCommand(cmd2)
+	assert.Equal(t, 2, len(mondas.CommandLine.Commands))
+	assert.Equal(t, cmd1.Name, mondas.CommandLine.Commands[0].Name)
+	assert.Equal(t, cmd2.Name, mondas.CommandLine.Commands[1].Name)
+}
