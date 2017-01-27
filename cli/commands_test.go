03084c19472a60cb2dad900d5dadcd149594373b
Jamie Connolly <76968+jamieconnolly@users.noreply.github.com>
2017-01-27 12:18:02 +0000

    :white_check_mark: Better testing of objects and slices

--- cli/commands_test.go
+++ cli/commands_test.go
@@ -84,9 +84,9 @@ func TestCommands_Add(t *testing.T) {
 
 	cmds := cli.Commands{cmd1}
 	cmds.Add(cmd2)
-	assert.Equal(t, 2, cmds.Len())
-	assert.Equal(t, cmd1.Name, cmds[0].Name)
-	assert.Equal(t, cmd2.Name, cmds[1].Name)
+	assert.Len(t, cmds, 2)
+	assert.Equal(t, cmd1, cmds[0])
+	assert.Equal(t, cmd2, cmds[1])
 }
 
 func TestCommands_Lookup(t *testing.T) {
@@ -94,8 +94,8 @@ func TestCommands_Lookup(t *testing.T) {
 	cmd2 := &cli.Command{Name: "two"}
 
 	cmds := cli.Commands{cmd1, cmd2}
-	assert.Equal(t, cmd1.Name, cmds.Lookup("one").Name)
-	assert.Equal(t, cmd2.Name, cmds.Lookup("two").Name)
+	assert.Equal(t, cmd1, cmds.Lookup("one"))
+	assert.Equal(t, cmd2, cmds.Lookup("two"))
 	assert.Nil(t, cmds.Lookup("three"))
 }
 
@@ -106,13 +106,13 @@ func TestCommands_SuggestionsFor(t *testing.T) {
 	cmds := cli.Commands{cmd1, cmd2, cmd3}
 
 	suggestions1 := cmds.SuggestionsFor("neo")
-	assert.Equal(t, 1, suggestions1.Len())
-	assert.Equal(t, cmd1.Name, suggestions1[0].Name)
+	assert.Len(t, suggestions1, 1)
+	assert.Equal(t, cmd1, suggestions1[0])
 
 	suggestions2 := cmds.SuggestionsFor("t")
-	assert.Equal(t, 2, suggestions2.Len())
-	assert.Equal(t, cmd3.Name, suggestions2[0].Name)
-	assert.Equal(t, cmd2.Name, suggestions2[1].Name)
+	assert.Len(t, suggestions2, 2)
+	assert.Equal(t, cmd3, suggestions2[0])
+	assert.Equal(t, cmd2, suggestions2[1])
 }
 
 func TestCommands_Visible(t *testing.T) {
@@ -122,7 +122,7 @@ func TestCommands_Visible(t *testing.T) {
 	cmds := cli.Commands{cmd1, cmd2, cmd3}
 
 	visible := cmds.Visible()
-	assert.Equal(t, 2, visible.Len())
-	assert.Equal(t, cmd1.Name, visible[0].Name)
-	assert.Equal(t, cmd3.Name, visible[1].Name)
+	assert.Len(t, visible, 2)
+	assert.Equal(t, cmd1, visible[0])
+	assert.Equal(t, cmd3, visible[1])
 }
