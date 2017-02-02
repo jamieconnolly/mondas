9f19b0be1a40e2516b568659ecded456dfe1b027
Jamie Connolly <jamie@jamieconnolly.com>
2017-02-02 15:29:27 +0000

    :sparkles: Allow executables to handle completions

--- completions/mondas.zsh
+++ completions/mondas.zsh
@@ -1,13 +1,18 @@
 #compdef mondas
 
-local cmds line ret=1 state
+local args cmds line ret=1 state
 
 _arguments -C \
   '--help[Display help information]' \
   '--version[Display version information]' \
-  '1: :->cmds' && ret=0
+  '1: :->cmds' \
+  '*: :->args' && ret=0
 
 case $state in
+  args)
+    args=(${(f)"$(_call_program arguments ${service} completions ${line[@]:0:$((CURRENT-1))} 2>/dev/null)"})
+    _values "${service} arguments" $args && ret=0
+    ;;
   cmds)
     cmds=(${(f)"$(_call_program commands ${service} completions 2>/dev/null)"})
     _describe -t commands "${service} commands" cmds && ret=0
